c40566c63874236ac3cdfe5d88ccceed6d5eb9fd
yosoykyle <valerian5953@gmail.com>
2025-05-06 16:15:08 +0800

    Initial commit

---
+++ src/main/java/com/espressionist_ecommerce/EspressionistEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.espressionist_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspressionistEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspressionistEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espressionist_ecommerce/EspressionistEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espressionist_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspressionistEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
